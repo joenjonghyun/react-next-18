4ea6e8c62c0a330ff6ac84e508afe77e06e69084
joenjonghyun <jjh980814@naver.com>
2022-04-27 10:08:54 +0900

    22.4.26

---
+++ _redux/reducers/articleReducer.ts
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit"
+// parameter
+
+// payload
+export interface ArticleType{
+    
+    title: string;
+     name: string;  
+     teamId: string;
+     subject: string;
+        }
+
+// state
+export interface ArticleState{
+    Loading: boolean;
+    data: ArticleType[];
+    error: any;
+    
+}
+
+
+const initialState: ArticleState = {
+    Loading: false,
+    data: [],
+    error: null
+}
+
+const articleSlice = createSlice({
+    name: 'articles',
+    initialState,
+    reducers: {
+        joinRequest: (state: ArticleState, payload)=>{
+             alert('진행2.리듀서내부')
+             state.Loading = true;
+            },
+        joinSuccess(state: ArticleState, {payload}){
+            state.data = [...state.data, payload]
+            state.Loading = false;
+        },
+        joinFailure(state: ArticleState, {payload}){ 
+            state.data = payload; 
+            state.Loading = false;
+        }
+    }
+})
+const { reducer, actions } = articleSlice
+export const articleActions = actions
+export default reducer
---
+++ store/sagas/user.js
@@ -0,0 +1,23 @@
+import { call, delay, put, takeLatest } from 'redux-saga/effects'
+import * as T from "@/store/types"
+
+
+
+function* addUser(action){
+    try{
+        alert(' >>> Saga 인지함 <<< ')
+        const newUser = yield response.json()
+        yield put({
+            type: T.USER_ADD_SUCCESSED,
+            payload: newUser.data
+        }) 
+    }catch(error){
+        yield put({
+            type: T.USER_ADD_FAILED,
+            payload: error.message
+        })
+    }
+}
+export function* watchAddUser(){
+    yield takeLatest(T.USER_ADD_REQUESTED, addUser)
+}
